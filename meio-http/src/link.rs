1002ba532c8eb928758f049a96bde42ea95c41d6
overfloat <rillrate@gmail.com>
2020-12-26 14:23:19 +0300

    Add ws_route method to the HttpServerLink

--- meio-http/src/link.rs
+++ meio-http/src/link.rs
@@ -1,8 +1,8 @@
 use super::HttpServer;
-use crate::server::{FromRequest, Req, Route, RouteImpl};
+use crate::server::{FromRequest, Req, Route, RouteImpl, WsReq, WsRouteImpl};
 use anyhow::Error;
 use derive_more::From;
-use meio::prelude::{Action, Actor, Address, InteractionHandler};
+use meio::prelude::{Action, ActionHandler, Actor, Address, InteractionHandler};
 use std::marker::PhantomData;
 
 #[derive(Debug, Clone, From)]
@@ -23,6 +23,21 @@ impl HttpServerLink {
         A: Actor + InteractionHandler<Req<E>>,
     {
         let route = RouteImpl {
+            extracted: PhantomData,
+            address,
+        };
+        let msg = AddRoute {
+            route: Box::new(route),
+        };
+        self.address.act(msg).await
+    }
+
+    pub async fn add_ws_route<E, A>(&mut self, address: Address<A>) -> Result<(), Error>
+    where
+        E: FromRequest,
+        A: Actor + ActionHandler<WsReq<E>>,
+    {
+        let route = WsRouteImpl {
             extracted: PhantomData,
             address,
         };
