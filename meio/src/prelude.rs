147081cc47487c813eaa4ecd61f5ec92bfc157bb
overfloat <rillrate@gmail.com>
2021-03-01 15:55:59 +0300

    Export InteractionDone

--- meio/src/prelude.rs
+++ meio/src/prelude.rs
@@ -3,8 +3,8 @@
 pub use crate::actor_runtime::{Actor, Context, Status};
 pub use crate::handlers::{
     Action, ActionHandler, Consumer, Eliminated, InstantAction, InstantActionHandler, Interaction,
-    InteractionHandler, InteractionResponder, InterruptedBy, Parcel, Scheduled, StartedBy,
-    TaskEliminated,
+    InteractionDone, InteractionHandler, InteractionResponder, InterruptedBy, Parcel, Scheduled,
+    StartedBy, TaskEliminated,
 };
 pub use crate::ids::{Id, IdOf};
 pub use crate::linkage::{ActionRecipient, Address, Distributor, InteractionRecipient};
